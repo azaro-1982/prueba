5999197a6f9dd72e65e883827256c32aabc8096f
David <d.azaro82@gmail.com>
2025-11-20 01:41:33 +0100

    primer commit

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,10 @@
-print("Hola mundo. Este es mi primer archivo en github")
+def saludo():
+    print("Hola mundo, soy una función para saludar")
+
+def otra_funcion(a, b):
+    return a + b
+
+saludo = saludo()
+suma = otra_funcion(5, -9)
+print(f"Esta función recibe la suma de dos variables")
+print(f"Estoy saludando desde una función que se llama saludo: {saludo}")
